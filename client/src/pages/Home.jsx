08977ca28bbce0a073a480e82cb72fcf9d4fd2e8
piyush-gupta123 <95485177+piyush-gupta123@users.noreply.github.com>
2023-03-23 22:22:24 +0530

    Work In Progress

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,59 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  height: 500px;
+  color: white;
+  position: sticky;
+  margin-top: 5%;
+  margin-left: 40%;
+`;
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  align-items: center;
+  border: 2px solid #ccc;
+  border-radius: 30px;
+  gap: 20px;
+  padding: 50px 70px;
+`;
 
+const ButtonDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 40%;
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+  background: skyblue;
+  padding: 15px 10px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 500;
+  margin: 20px 0px;
+`;
 
 const Home = () => {
   return (
     <Container>
-      
+      <Wrapper>
+        <ButtonDiv>
+          <Button>New Meeting</Button>
+          <Button>Join Meeting</Button>
+        </ButtonDiv>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
